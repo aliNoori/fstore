3476f07286baa6d951a82989bdb20519b74bb392
Ali Noori <matineali@gmail.com>
2025-06-09 07:09:20 +0330

    new index

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -8,11 +8,12 @@ export default defineNuxtPlugin((nuxtApp) => {
         //baseURL: 'http://192.168.1.100:8000/api/', // آدرس API پیش‌فرض شما
         //baseURL: 'http://192.168.1.105/api/', // آدرس API پیش‌فرض شما
         //baseURL: 'http://185.204.197.237/store/api/',
-        baseURL: 'https://nemoonehshow.ir/store/api/',
+        //baseURL: 'https://nemoonehshow.ir/store/api/',
         //baseURL: 'http://store.laravel.local/api/', // آدرس API پیش‌فرض شما
         //baseURL:'http://store.pythone.local/api/',//آدرس API پیش‌فرض شما
         //withCredentials: true // اطمینان از ارسال و دریافت کوکی‌ها
         //baseURL: 'https://store.python.local/api/', // آدرس API پیش‌فرض شما
+        baseURL: 'https://linkuapi.petoman.com/api/', // Base URL for the API
     })
     api.interceptors.request.use(config => {
             const authStore = useAuthStore()
